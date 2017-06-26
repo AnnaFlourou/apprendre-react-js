8695260b6c00b512f2f82b2b0b836d75fd811695
Hassane Moustapha <hassanemoustapha@gmail.com>
2017-06-26 20:54:44 +0000

    app structure & multi components usage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
+import List from './components/list';
+import TodoForm from './components/todoForm';
 import './App.css';
 
-class List extends Component {
-  render(){
-    return(
-      <div className="liste">ici c'est ma liste</div>
-    );
-  }
-}
-
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      todos: []
+    };
+  }
+
+  onNewTodo(todo){
+    let newTodoList = this.state.todos;
+    newTodoList.push(todo);
+    this.setState({ todos: newTodoList });
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Apprendre ReactJS</h2>
+          <h2>TodoApp</h2>
         </div>
-        <p className="App-intro">
-          <List />
-        </p>
+        <div className="App-intro">
+          <TodoForm onNewTodo={this.onNewTodo.bind(this)}/>
+          <List todos={this.state.todos}/>
+        </div>
       </div>
     );
   }
