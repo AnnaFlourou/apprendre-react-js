54005c2db170e825982268e95e07ddf2a6865c8d
Hassane Moustapha <hassanemoustapha@gmail.com>
2017-07-02 08:28:52 +0000

    Added 'interaction'

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -3,7 +3,28 @@ import React, { Component } from 'react';
 class List extends Component {
   render(){
     return(
-      <div className="liste">{this.props.todos.length}</div>
+      <div className="liste">
+        Todos: [{this.props.todos.length}]
+        { this.showTodos(this.props.todos) }
+      </div>
+    );
+  }
+
+  toggleTodo(todo){
+    console.log("todo clicked", todo);
+  }
+
+  showTodos(todos){
+    return(
+      todos.map((todo) => {
+        return(
+          <div className="todo"
+            key="todo-{todo.title}"
+            onClick={() => this.toggleTodo(todo)}>
+            {todo.title} {todo.done ? 'oui' : 'non'}
+          </div>
+        )
+      })
     );
   }
 }
